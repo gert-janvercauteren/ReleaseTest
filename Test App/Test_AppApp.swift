3e9542d78fedcc054eb818d8ea1f3dad2e457aab
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2023-03-21 15:31:53 +0800

    first commit

---
+++ Test App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test App
+//
+//  Created by Gert-Jan Vercauteren on 2023/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
